2dc09cfc9ff57a9045b6521fc6f773cbfc7f6b04
Štěpán Velínský <stteranv@gmail.com>
2024-10-01 15:58:31 +0200

    Company Pages

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -3,6 +3,9 @@ import { useParams } from 'react-router'
 import { CompanyProfile } from '../../company';
 import { getCompanyProfile } from '../../api';
 import { get } from 'http';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import CompanyDashboard from '../../Components/CompanyDashboard/CompanyDashboard';
+import Title from '../../Components/Title/Title';
 
 interface Props {}
 
@@ -20,7 +23,11 @@ const CompanyPage = (props: Props) => {
   }, [])
   return <>
     {company ? (
-      <div>{company.companyName}</div>
+      <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+      <Sidebar />
+      <CompanyDashboard><Title title="Company Name" subTitle={company.companyName}></Title></CompanyDashboard>
+      
+    </div>
     ) : (
       <div>Company not found!</div>
     )}
