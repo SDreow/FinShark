9d73aa0dbd0195711688ab9c46aa3b9f03fb39a2
Štěpán Velínský <stteranv@gmail.com>
2024-10-04 22:57:15 +0200

    Create Database + migration

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc915f7026613ae89caac6b2961087fa30ee0c62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e40384d18e5b9cd5cd7562fc4aa6d9c5f71b503")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
